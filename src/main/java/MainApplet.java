9622f347abd296ccc1a8674db191deed47ea54fc
shan91220 <shan91220@gmail.com>
2016-05-12 13:35:55 +0800

    change MainApplet and Network

--- src/main/java/MainApplet.java
+++ src/main/java/MainApplet.java
@@ -1,6 +1,16 @@
 package main.java;
 
+import java.awt.Color;
+import java.util.ArrayList;
+
+import controlP5.Button;
+import controlP5.CColor;
+import controlP5.ControlFont;
+import controlP5.ControlP5;
+import de.looksgood.ani.Ani;
 import processing.core.PApplet;
+import processing.core.PFont;
+import processing.data.*;
 
 /**
 * This class is for sketching outcome using Processing
@@ -9,24 +19,158 @@ import processing.core.PApplet;
 @SuppressWarnings("serial")
 public class MainApplet extends PApplet{
 	private String path = "main/resources/";
-	private String file = "starwars-episode-1-interactions.json";
+	private ArrayList<String> file = new ArrayList<String>();
+	private ArrayList<Network> net = new ArrayList<Network>();
+	private Network currentNet;
+	private int episode;
+	private PFont font;
+	
+	// UI
+	private ControlP5 button;
+	private Button addAll;
+	private Button clear;
 	
 	private final static int width = 1200, height = 650;
 	
 	public void setup() {
-
+		
 		size(width, height);
-		smooth();
+		
 		loadData();
+		smooth();
+		initial();
+		buttons();
+
+		Ani.init(this);
 		
 	}
-
+	public void buttons(){
+		button = new ControlP5(this);		
+		button.setColorBackground( color(0,255,255) );
+		addAll = button.addButton("addAll") .setLabel("Add All").setPosition(950, 150) .setSize(200, 70);
+		addAll.getCaptionLabel().setFont(new ControlFont(createFont("Verdana",20)));
+		clear = button.addButton("clear") .setLabel("Clear").setPosition(950, 250) .setSize(200, 70);
+		clear.getCaptionLabel().setFont(new ControlFont(createFont("Verdana",20)));
+	}
+	public void addAll()
+	{
+		for(Character ch : currentNet.characters)
+			if(!this.currentNet.circleCh.contains(ch))
+			{
+				ch.setShowLink(1);
+				this.currentNet.addOne(ch);	
+			}
+				
+		
+		for(Character ch : currentNet.characters) 
+		{
+			
+			ch.setState(4);
+			Ani ani = Ani.to(ch,1.3f,"x",ch.getCX());
+			Ani.to(ch,1.3f,"y",ch.getCY());
+			
+			ani.setCallback("onEnd:addAniDone");
+		}	
+	}
+	public void clear()
+	{
+		for(Character ch : currentNet.characters)
+		{
+			if(this.currentNet.circleCh.contains(ch))
+			{
+				this.currentNet.removeOne(ch);
+				ch.setShowLink(0);
+				
+			
+				ch.setState(4);
+				Ani ani = Ani.to(ch,1.3f,"x",ch.getOGX());
+				Ani.to(ch,1.3f,"y",ch.getOGY());
+				
+				ani.setCallback("onEnd:removeAniDone");
+			}
+		}	
+	}
+	public void initial(){
+		currentNet = net.get(0);
+		episode = 1;
+		font = createFont("Arial", 50);
+	}
 	public void draw() {
+		background(255);
+		currentNet.display();
+		
+		fill(0,50,0);
 
+		textFont(font);
+		text("Star Wars " + episode,450,50);
 	}
-
+	
+	public void keyPressed()
+	{
+		if(key > '0' && key < '8')	
+		{
+			currentNet = net.get(key - '1');
+			episode = key - '0';
+		}
+	}
+	
 	private void loadData(){
+		int k, a, b;
+		for(int i=1; i<8; i++){
+			file.add("starwars-episode-"+i+"-interactions.json");
+			net.add(new Network(this));
+		}
+		for(int i=0; i<7; i++){
+			currentNet = net.get(i);
+			JSONObject json = loadJSONObject(path + file.get(i));
+			JSONArray nodes = json.getJSONArray("nodes");
+			JSONArray links = json.getJSONArray("links");
+			
+			for(int j=0; j<nodes.size(); j++){
+				JSONObject tmp = nodes.getJSONObject(j);
+				String name = tmp.getString("name");
+				String colour = tmp.getString("colour");
+			    k = Integer.parseInt(colour.substring(3,5),16);
+				a = Integer.parseInt(colour.substring(5,7),16);
+				b = Integer.parseInt(colour.substring(7,9),16);
+				
+				Character ch = new Character(this,name,0,0,k,a,b);
+				
 
+				currentNet.characters.add(ch);
+			}
+			
+			setChXY();
+			
+
+			for(int j=0 ; j<links.size() ; j++)
+			{
+				JSONObject link = links.getJSONObject(j);
+				int source = link.getInt("source");
+				int target = link.getInt("target");
+				Integer value = link.getInt("value");
+				
+
+				Character src = currentNet.characters.get(source);
+				src.addTarget(currentNet.characters.get(target), value);
+			}
+		}
 	}
-
+	public void setChXY()
+	{
+		int ogx = 30,ogy = 50;
+		for(Character ch : currentNet.characters)
+		{
+			ch.setOGX(ogx);
+			ch.setOGY(ogy);
+			ch.setState(3);
+			ogx += 80;
+			if(ogx > 320)
+			{
+				ogy += 50;
+				ogx = 30;
+			}
+		}
+	}
+	
 }
--- src/main/java/Network.java
+++ src/main/java/Network.java
@@ -1,5 +1,9 @@
 package main.java;
 
+import java.util.ArrayList;
+
+import controlP5.ControlP5;
+import de.looksgood.ani.Ani;
 import processing.core.PApplet;
 
 /**
@@ -10,16 +14,122 @@ import processing.core.PApplet;
 */
 public class Network {
 	
+	private int x;
+	private int y;
 	private PApplet parent;
-
+	ArrayList<Character> characters;
+	 ArrayList<Character> circleCh;
+	private int diameter;
+	private int mouse = 0;
+	private double pos = 0;
+	
 	public Network(PApplet parent){
 
 		this.parent = parent;
+		this.characters = new ArrayList<Character>();
+		this.circleCh = new ArrayList<Character>();
 		
-	}
-
-	public void display(){
-		
+		this.x = 600;
+		this.y = 300;
+		this.diameter = 530;
 	}
 	
+	public void removeOne(Character ch)
+	{
+		circleCh.remove(ch);
+		pos = 0;
+		for(Character cha : circleCh){
+			cha.setCX((float)(x + diameter/2*Math.cos(Math.toRadians(pos))));
+			cha.setCY((float)(y + diameter/2*Math.sin(Math.toRadians(pos))));
+			pos += (double)360 / (double)circleCh.size();
+		}
+	}
+	
+	
+	
+	public void drawLink()
+	{
+		for(Character ch : circleCh)
+		{
+			for(Character tar: ch.getTarget().keySet())
+			{
+				if(tar.getShowLink() == 1)
+				{
+					int intense = ch.getTarget().get(tar);
+					parent.strokeWeight(intense / 2);
+					parent.stroke(0,77,173,250);
+					parent.line(ch.getX(), ch.getY(), tar.getX(), tar.getY());
+				}
+			}
+		}
+	}
+	public void addOne(Character ch)
+	{	
+		circleCh.add(ch);
+		pos = 0;
+		for(Character cha : circleCh){
+			cha.setCX((float)(x + diameter/2*Math.cos(Math.toRadians(pos))));
+			cha.setCY((float)(y + diameter/2*Math.sin(Math.toRadians(pos))));
+			pos += (double)360 / (double)circleCh.size();
+		}
+	}
+	public void display(){
+		parent.stroke(204,250,0);
+		parent.strokeWeight(4);
+		parent.fill(255);
+		parent.ellipse(x,y,diameter,diameter);
+		
+	
+		for(Character ch : characters)
+		{
+			if((parent.mouseX <= ch.getX()+20 && parent.mouseX >= ch.getX()-20) && (parent.mouseY <= ch.getY()+20 && parent.mouseY >= ch.getY()-20))
+			{
+				ch.setShowName(1);
+			
+				if(parent.mousePressed == true)
+				{
+					if(mouse==0)
+					{
+						mouse = 1;
+						ch.setState(1);
+					}
+				}
+
+				else
+				{
+
+					if(ch.getState() == 1)
+					{
+						if((ch.getX()-x)*(ch.getX()-x) + (ch.getY()-y)*(ch.getY()-y)- diameter*diameter/4 < 0.01)
+						{
+							ch.setState(2);
+							ch.setShowLink(1);
+							
+							if(!circleCh.contains(ch))
+								addOne(ch);
+						}
+		
+						else
+						{
+							ch.setState(3);
+							ch.setShowLink(0);
+							removeOne(ch);
+						}
+						pos = 0;
+						for(Character cha : circleCh){
+							cha.setCX((float)(x + diameter/2*Math.cos(Math.toRadians(pos))));
+							cha.setCY((float)(y + diameter/2*Math.sin(Math.toRadians(pos))));
+							pos += (double)360 / (double)circleCh.size();
+						}
+					}
+				
+					mouse = 0;
+				}
+			}
+			else ch.setShowName(0);	
+			ch.display();
+		}
+		
+		drawLink();
+	}
 }
